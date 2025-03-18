1fe0bf54df3d055eb0d04116431afe628976a9a2
klemthiago <tpklem@gmail.com>
2025-03-18 07:53:15 -0300

    add screenshot

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,13 @@ function App() {
   const formatador = new Intl.NumberFormat("pt-BR", {
     maximumFractionDigits: 2,
   });
+  const [frase, setFrase] = useState("");
+  const [data, setData] = useState("");
 
   useEffect(() => {
     async function fetchTasks() {
       const response = await fetch(
-        "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL",
+        "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,JPY-BRL,GBP-BRL,CNY-BRL,AUD-BRL,CAD-BRL,CHF-BRL,BTC-BRL,ETH-BRL,SOL-BRL",
         { method: "GET" }
       );
       const data = await response.json();
@@ -43,19 +45,35 @@ function App() {
     const result = Object.entries(moedas).filter(
       ([key, value]) => key == filterData
     );
-    console.log(result[0]);
+    //console.log(result[0]);
     setSelectedValue(result[0][1]);
-    console.log(selectedValue);
+    //console.log(selectedValue);
+    setFrase(
+      `1 ${result[0][1].code} é igual a ${formatador.format(
+        result[0][1].bid
+      )} ${result[0][1].codein}`
+    );
+    const dataFinal = new Date(result[0][1].create_date);
+    setData(
+      `Data da cotação: ${dataFinal.toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false,
+      })}`
+    );
   }
 
   const handleKeyDown = (event) => {
     setValorInputOrigem(event.target.value);
-    console.log(valorInputOrigem);
   };
 
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-gradient-to-br from-sky-50 to-gray-200">
-      <div className="w-[400px] h-[400px] rounded-xl bg-white shadow-xl p-10 space-y-4">
+      <div className="w-[400px] rounded-xl bg-white shadow-xl p-10 space-y-4">
         <div className="w-full text-blue-500 flex space-x-3 justify-center text-2xl">
           <CircleDollarSign size={32} />
           <div className="font-bold">Exchange App</div>
@@ -69,15 +87,21 @@ function App() {
           </option>
           {opcoes}
         </select>
-        <div className="w-full border border-blue-200 text-gray-700 flex">
+        <div
+          className={`w-full border border-blue-200 text-gray-700 flex ${
+            frase ? "" : "hidden"
+          }`}
+        >
           <div className="text-1xl text-blue-400 text-right w-full">
-            <p className="py-2 px-4 text-center">
-              1 {selectedValue.code} é igual a{" "}
-              {formatador.format(selectedValue.bid)} {selectedValue.codein}
-            </p>
+            <p className="px-4 text-center font-bold">{frase}</p>
+            <p className="px-4 text-center">{data}</p>
           </div>
         </div>
-        <div className="w-full border border-blue-200 text-gray-700 flex">
+        <div
+          className={`w-full border border-blue-200 text-gray-700 flex ${
+            frase ? "" : "hidden"
+          }`}
+        >
           <div className="text-3xl font-bold bg-blue-400 text-white text-center">
             <p className="py-2 px-4 w-[100px]">{selectedValue.code}</p>
           </div>
@@ -89,7 +113,11 @@ function App() {
             />
           </div>
         </div>
-        <div className="w-full border border-blue-200 text-gray-700 flex">
+        <div
+          className={`w-full border border-blue-200 text-gray-700 flex ${
+            frase ? "" : "hidden"
+          }`}
+        >
           <div className="text-3xl font-bold bg-blue-400 text-white text-center ">
             <p className="py-2 px-4 w-[100px]">{selectedValue.codein}</p>
           </div>
@@ -98,7 +126,11 @@ function App() {
               disabled
               type="text"
               className="py-2 px-4 w-full outline-0 bg-slate-100"
-              value={formatador.format(valorInputOrigem * selectedValue.bid)}
+              value={
+                selectedValue.bid
+                  ? formatador.format(valorInputOrigem * selectedValue.bid)
+                  : ""
+              }
             />
           </div>
         </div>
